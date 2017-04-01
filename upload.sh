7cdaf6d943c6377fcba0c147b076d954024d3321
Alex Recker <alex@reckerfamily.com>
2017-04-01 14:33:46 -0500

    More code

--- upload.sh
+++ upload.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env bash
+
+# Thumbify Images
+for file in $(ls ./images/ -I "logo.png" -I "banner.jpg" -I "thumb-*"); do
+    convert ./images/${file} \
+	    -crop 400x400+0+0 \
+	    -gravity center \
+	    -resize 400x400 \
+	    -extent 400x400 \
+	    ./images/thumb-${file}
+done
+
+# Build Site
 bundle exec jekyll build
-cd ./_site
-aws s3 sync . s3://astuaryart.com/
+
+# Upload to S3
+cd ./_site && aws s3 sync . s3://astuaryart.com/
