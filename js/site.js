a977e6efd7c17189edfab72333362e4d6a4627d3
Alex Recker <alex@reckerfamily.com>
2017-05-30 20:53:33 -0500

    Adds some of the filter bar

--- js/site.js
+++ js/site.js
@@ -1,3 +1,16 @@
+function tagClickHandler(event) {
+  var clicked = $(event.target).attr('href').replace('#', '');
+  $('.gallery').show();
+  if (clicked === 'tag-all') return;
+  $.each($('.tag'), function() {
+    var target = $(this).attr('href').replace('#', '');
+    if (target !== clicked) {
+      $('.' + target).hide();
+    }
+  });
+}
+
 $(function(){
   $('.big-image').abigimage();
+  $('.tag').click(tagClickHandler);
 });
